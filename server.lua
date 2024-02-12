07fc9bf4b034495061262f91f95044ee96688ed0
Henk Wietsma <151195189+HenkW00@users.noreply.github.com>
2024-02-12 02:26:26 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -4,8 +4,8 @@ local queueActive = false
 local queueTime = 10
 local queuePosition = 0
 
-RegisterServerEvent('queue:notify')
-AddEventHandler('queue:notify', function(message)
+RegisterServerEvent('hw_waiting:notify')
+AddEventHandler('hw_waiting:notify', function(message)
     TriggerClientEvent('chatMessage', -1, '^1Queue System^7', {255, 255, 255}, message)
 end)
 
@@ -42,15 +42,35 @@ RegisterCommand('startqueue', function(source, args, rawCommand)
         queuePosition = 0
         TriggerClientEvent('hw_waiting:notify', -1, "Queue is now active. Estimated wait time: " .. queueTime .. " seconds.")
     else
-        print("Queue is already active.")
+        print("^0[^1ERROR^0]: Queue is already ^2active^0.")
     end
 end, true)
 
 RegisterCommand('stopqueue', function(source, args, rawCommand)
     if queueActive then
         queueActive = false
-        TriggerClientEvent('queue:notify', -1, "Queue is now inactive.")
+        TriggerClientEvent('hw_waiting:notify', -1, "Queue is now inactive.")
     else
-        print("Queue is not active.")
+        print("^0[^1ERROR^0]: Queue is ^1not^0 active! For help use ^5/queuehelp")
     end
 end, true)
+
+RegisterCommand('queueinfo', function(source, args, rawCommand)
+    if queueActive then
+        queueTime = tonumber(args[1] or queueTime)
+        TriggerClientEvent('hw_waiting:notify', -1, "Wachtrij ingesteld op:" ..queueTime .. " seconden.")
+        print("^0[^2INFO^0]: Queue time is: ^5" .. queueTime .. "^0 seconds")
+    else
+        print("^0[^1ERROR^0]: Queue is ^1not^0 active! For help use ^5/queuehelp")
+    end
+end, true)
+
+RegisterCommand('queuehelp', function(source, args, rawCommand)
+    if queueActive then
+        print("^0[^2INFO^0]: Use ^5/startqueue^0 to activate the queue. U can configure the time at ^5line 4^0 in server.lua")
+    else
+        print("^0[^2INFO^0]: Use ^5/startqueue^0 to activate the queue. U can configure the time at ^5line 4^0 in server.lua")
+    end
+end, true)
+
+
