78134ffe931cae9893892cbc17e23f1c86b5c377
Henk Wietsma <151195189+HenkW00@users.noreply.github.com>
2024-02-12 02:33:29 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,9 +3,10 @@ game 'gta5'
 
 author 'HenkW'
 description 'Waiting script'
-version '1.0.2'
+version '1.0.3'
 
 client_script 'client.lua'
 server_script 'server.lua'
+server_script 'version.lua'
 
 shared_script '@es_extended/imports.lua'
